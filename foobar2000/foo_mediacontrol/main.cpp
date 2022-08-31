2ccc6a5b7e53775aecf15da36a2dfe659093127a
WindowsLogic <admin@windowslogic.co.uk>
2022-08-31 16:02:52 +0100

    Updated SDK.

--- foobar2000/foo_mediacontrol/main.cpp
+++ foobar2000/foo_mediacontrol/main.cpp
@@ -1,4 +1,4 @@
 #include "stdafx.h"
 
-DECLARE_COMPONENT_VERSION("Luna Music Rich Presence","1.0","Luna Music for foobar2000.");
+DECLARE_COMPONENT_VERSION("Caitlin's Rich Presence", "1.0", "Caitlin's Music for foobar2000.");
 VALIDATE_COMPONENT_FILENAME("foo_discord.dll");
